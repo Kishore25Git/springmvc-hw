d2d580deb74593ba494578a2c1d6492ffab2b799
Kishore25Git <akishore25@gmail.com>
2025-07-19 12:53:04 +0800

    first commit of springmvc hw

---
+++ src/main/java/com/springmvc/hw/main/SpringmvcHwApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.hw.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc/hw/main/SpringmvcHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.hw.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
